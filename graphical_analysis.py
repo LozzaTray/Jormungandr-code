cae277218ec8c512d69b87ec39a7af66461c010d
Lawrence Tray <ltray99@gmail.com>
2020-11-20 10:50:56 +0000

    Implement ABP

--- graphical_analysis.py
+++ graphical_analysis.py
@@ -1,17 +1,20 @@
 from data.facebook import FacebookGraph
 from typing import List
 from hypothesis.test_statistics import two_samples_mean_ll_ratio, students_z_test
+from model.graph import Graph
 
 
 def run():
-    graph = FacebookGraph(0)
-    graph.hypothesis_test_threeway(77)
-    #graph.hypothesis_test_keyword("gender")
-    #graph.hypothesis_test_keyword("birthday")
-    #graph.hypothesis_test_keyword("first_name")
-    #graph.hypothesis_test_keyword("last_name")
-    #graph.hypothesis_test_keyword("hometown")
-    #graph.hypothesis_test_keyword("language") # not disjoint
+    fb = FacebookGraph(0)
+    #fb.hypothesis_test_threeway(77)
+    #fb.hypothesis_test_keyword("gender")
+    #fb.hypothesis_test_keyword("birthday")
+    #fb.hypothesis_test_keyword("first_name")
+    #fb.hypothesis_test_keyword("last_name")
+    #fb.hypothesis_test_keyword("hometown")
+    #fb.hypothesis_test_keyword("language") # not disjoint
+    graph = Graph(fb.edges)
+    graph.abp()
 
 
 if __name__ == "__main__":
--- model/graph.py
+++ model/graph.py
@@ -1,4 +1,6 @@
 import numpy as np
+from tqdm import tqdm
+import math
 
 
 class Graph:
@@ -11,7 +13,7 @@ class Graph:
         vertex_set = origin_vertices.union(destin_vertices)
         
         # constants
-        self.N = len(self.vertex_set)
+        self.N = len(vertex_set)
         self.M = len(edges)
 
         # relabel vertices with an index
@@ -22,23 +24,24 @@ class Graph:
             self.index_to_vertex[index] = vertex
             self.vertex_to_index[vertex] = index
 
-        self.adjacency_list = [[] for v in range(0, self.N)]
+        self.adjacency_list = [set() for v in range(0, self.N)]
         self.adjacency_matrix = np.zeros((self.N, self.N))
 
         for edge in edges:
             a = self.vertex_to_index.get(edge[0])
             b = self.vertex_to_index.get(edge[1])
 
-            self.adjacency_list[a].append(b)
-            self.adjacency_list[b].append(a)
+            self.adjacency_list[a].add(b)
+            self.adjacency_list[b].add(a)
 
             self.adjacency_matrix[a, b] += 1
             self.adjacency_matrix[b, a] += 1
 
 
     def abp(self):
-        r = 3 # max length of cycle to correct for
-        T = 1000 # num_iters
+        # implement for r = 3
+        r = 3
+        T = math.floor(math.log2(self.N)) # num_iters - r
 
         y = np.zeros((T, self.N, self.N)) # y[t, i, j] is message at time t from i -> j
         z = np.zeros((T, self.N, self.N)) # z[t, i, j] is normalised message at t from i -> j
@@ -46,12 +49,38 @@ class Graph:
         # random initial setting
         y[r-1, :, :] = np.random.normal(size=(self.N, self.N))
 
-        for t in range(r, T):
-            for v in range(0, self.N):
-                adjacent_vertices = self.adjacency_list[v]
+        for t in tqdm(range(r, T)):
+            s = np.sum(y[t-1, :, :]) / (2 * self.M)
+            z[t-1, :, :] = y[t-1, :, :] - s
 
-                for vd in adjacent_vertices:
-                    y_vd_to_v = 
+            for v in range(0, self.N):
+                vd_set = self.adjacency_list[v]
+                
+                for vd in vd_set:
+                    y_vd_to_v = 0
+                    vdd_set = self.adjacency_list[vd].difference({v})
+
+                    for vdd in vdd_set:
+                        y_vd_to_v += z[t-1, vdd, vd]
+
+                        if vdd in vd_set:
+                            # part of 3-cycle
+                            y_vd_to_v -= z[t-3, vdd, v]
+                    
+                    y[t, vd, v] = y_vd_to_v
+
+        output = np.zeros(self.N)
+        for v in range(0, self.N):
+            vd_set = self.adjacency_list[v]
+
+            for vd in vd_set:
+                output[v] += y[T-1, vd, v]
+
+        print(np.mean(np.abs(output)))
+        self.assignments = (output > 0).astype(int)
+        print(self.assignments)
+
+
 
 
 
